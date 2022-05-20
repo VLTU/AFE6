a6a79ca668f3bba9cd48d41da9c0ffd91b7e13cb
VLTU <vincent.lechner1tu@gmail.com>
2022-05-20 10:56:45 +0200

    Verifiied Imap_derivatives and map

--- Exercise_1.m
+++ Exercise_1.m
@@ -1,7 +1,7 @@
 
 %Setting parameters 
 %quadrature points in single dimension Note: ref has nq = nq_1^2
-nq_1 = 2;
+nq_1 = 3;
 %polynomial bi-degree 
 p = [2,2];
 
@@ -11,3 +11,5 @@ ref_data = create_ref_data(nq_1,p,'plot')
 %geometry_file.fe_geometry.map_coefficients
 
 A = create_geometric_map(geometry_file.fe_geometry,ref_data);
+%A.map
+A.map_derivatives
--- create_geometric_map.m
+++ create_geometric_map.m
@@ -30,26 +30,37 @@ end
 %Generating the inverse map derivative:
 %imap derivative row j contains [dphi_inv_1/dx_1,dphi_inv_2/dx_1,dphi_inv_1/dx_2,dphi_inv_2/dxi2]
 %Generating determinant for inverse jacobian
-determinant = zeros(size(ref_data.evaluation_points,1),1,fe_geometry.m)
+determinant = zeros(size(ref_data.evaluation_points,1),1,fe_geometry.m);
 imap_derivatives = zeros(size(ref_data.evaluation_points,1),4,fe_geometry.m);
+Jac_2D = zeros(2,2);
 
-for k = 1:1
+for k = 1:fe_geometry.m
+    %Check for first evaluation point for verification purposes
+    %Jac_2D(1,1) = map_derivatives(1,1,k);
+    %Jac_2D(2,1) = map_derivatives(1,2,k);
+    %Jac_2D(1,2) = map_derivatives(1,3,k);
+    %Jac_2D(2,2) = map_derivatives(1,4,k);
+    %Jac_2D = inv(Jac_2D);
+    %Jac_2D(:).'
+    
+    
     %Filling Jacobian determinant 
-    determinant(:,1,k) = map_derivatives(:,1,k) .* map_derivatives(:,4,k) - map_derivatives(:,2,k) .* map_derivatives(:,3,k)
+    determinant(:,1,k) = map_derivatives(:,1,k) .* map_derivatives(:,4,k) - map_derivatives(:,2,k) .* map_derivatives(:,3,k);
 
-
+    
     %Generating derivatives (Inverse jacobian)
-    dinv_11 = 1./determinant(:,1,k) .* map_derivatives(:,4,k)
-    dinv_21 = -1./determinant(:,1,k) .* map_derivatives(:,2,k)
-    dinv_12 = -1./determinant(:,1,k) .* map_derivatives(:,3,k)
-    dinv_22 =  1./determinant(:,1,k) .* map_derivatives(:,1,k)
+    dinv_11 = 1./determinant(:,1,k) .* map_derivatives(:,4,k);
+    dinv_21 = -1./determinant(:,1,k) .* map_derivatives(:,2,k);
+    dinv_12 = -1./determinant(:,1,k) .* map_derivatives(:,3,k);
+    dinv_22 =  1./determinant(:,1,k) .* map_derivatives(:,1,k);
     
     %Filling derivatives
-    imap_derivatives(:,1,k) = dinv_11
-    imap_derivatives(:,2,k) = dinv_21
-    imap_derivatives(:,3,k) = dinv_12
-    imap_derivatives(:,4,k) = dinv_22
-    
+    imap_derivatives(:,1,k) = dinv_11;
+    imap_derivatives(:,2,k) = dinv_21;
+    imap_derivatives(:,3,k) = dinv_12;
+    imap_derivatives(:,4,k) = dinv_22;
+    imap_derivatives(1,:,k);
+
 end 
 
 
